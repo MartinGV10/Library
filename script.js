bf082a57e11af3aa8d4a02925d91f0440de8bd0f
MartinGV10 <mganen10@gmail.com>
2025-08-14 09:24:10 -0400

    Final

--- script.js
+++ script.js
@@ -1,108 +1,102 @@
 // open dialog
 const dialog = document.querySelector('dialog')
 const showBtn = document.querySelector('.add')
+const closeBtn = document.querySelector('.close')
 
-// showBtn.addEventListener('click', () => {
-//     dialog.showModal()
-// })
+showBtn.addEventListener('click', () => {
+    dialog.showModal()
+})
 
+closeBtn.addEventListener('click', () => {
+    dialog.close()
+})
 
-function Book(name, author, pages, read) {
+function Book(name, author, pages, read, bookID) {
     this.name = name
     this.author = author
     this.pages = pages
     this.read = read
+    this.bookID = bookID
 }
 
-const orwell = new Book('1984', 'George Orwell', '329', 'Finished')
-
-
 const library = []
-
 
 const titleBox = document.querySelector('.tit')
 const authorBox = document.querySelector('.auth')
 const pageBox = document.querySelector('.page')
 const readBox = document.querySelector('.read')
-// const submitBtn = document.querySelector('.submit')
 const form = document.querySelector("form")
-form.addEventListener('submit', addToLib())
+form.addEventListener('submit', addToLib)
 
-function addToLib() {
-    form.addEventListener('submit', (e) => {
-        // dialog.close()
-        e.preventDefault()
-        const tit = titleBox.value
-        const auth = authorBox.value
-        const page = pageBox.value
-        const status = readBox.value
+function addToLib(e) {
+    e.preventDefault()
+    const tit = titleBox.value
+    const auth = authorBox.value
+    const page = pageBox.value
+    const status = readBox.value
+    const isbn = crypto.randomUUID()
 
-        const newBook = new Book(tit, auth, page, status)
+    const newBook = new Book(tit, auth, page, status, isbn)
 
-        library.push(newBook)
-        console.log(library)
+    library.push(newBook)
+    console.log(library)
 
-        displayBook(tit, auth, page, status)
-    })
+    dialog.close()
+    displayBook(newBook)
+    form.reset()
 }
 
-function displayBook(newTitle, newAuth, newPage, newStatus) {
-    // const titleDisp = document.querySelector('.title')
-    // const authorDisp = document.querySelector('.author')
-    // const numDisp = document.querySelector('.num')
-    // const statusDisp = document.querySelector('.status')
-
+function displayBook(newBook) {
     const bookCont = document.querySelector('.book-cont')
-    // const oneBook = document.querySelector('.book')
-    // const groupA = document.querySelector('.grp1')
-    // const groupB = document.querySelector('.grp2')
 
-    library.forEach(() => {
-        const content = document.createElement('div')
-        content.classList.add('book')
-        bookCont.appendChild(content)
+    const content = document.createElement('div')
+    content.classList.add('book')
+    content.dataset.id = newBook.bookID
+    bookCont.appendChild(content)
 
-        const grpA = document.createElement('div')
-        grpA.classList.add('grp1')
-        content.appendChild(grpA)
+    const grpA = document.createElement('div')
+    grpA.classList.add('grp1')
+    content.appendChild(grpA)
 
-        const grpB = document.createElement('div')
-        grpB.classList.add('grp2')
-        content.appendChild(grpB)
+    const grpB = document.createElement('div')
+    grpB.classList.add('grp2')
+    content.appendChild(grpB)
 
+    const nameBox = document.createElement('h3')
+    nameBox.classList.add('title')
+    grpA.appendChild(nameBox)
+    
+    const authBox = document.createElement('h4')
+    authBox.classList.add('author')
+    grpA.appendChild(authBox)
+    
+    const numBox = document.createElement('p')
+    numBox.classList.add('num')
+    numBox.textContent = `{$book.pages} pages`
+    grpB.appendChild(numBox)
+    
+    const statBox = document.createElement('p')
+    statBox.classList.add('status')
+    grpB.appendChild(statBox)
 
-        const nameBox = document.createElement('h3')
-        nameBox.classList.add('title')
-        grpA.appendChild(nameBox)
-        
-        const authBox = document.createElement('h4')
-        nameBox.classList.add('author')
-        grpA.appendChild(authBox)
-        
-        const numBox = document.createElement('p')
-        nameBox.classList.add('num')
-        grpB.appendChild(numBox)
-        
-        const statBox = document.createElement('p')
-        nameBox.classList.add('status')
-        grpB.appendChild(statBox)
+    const newDel = document.createElement('button')
+    newDel.textContent = 'Delete'
+    newDel.classList.add('delete')
+    grpB.appendChild(newDel)
 
-        const newDel = document.createElement('button')
-        newDel.textContent = Delete
-        newDel.classList.add('delete')
-        grpB.appendChild(newDel)
-
-        nameBox.textContent = newTitle
-        authBox.textContent = newAuth
-        numBox.textContent = newPage
-        statBox.textContent = newStatus
-
-        
-    }) 
+    nameBox.textContent = newBook.tit
+    authBox.textContent = newBook.author
+    numBox.textContent = `${newBook.pages} pages`
+    statBox.textContent = `Status: ${newBook.read}`
+    
+    newDel.addEventListener('click', () => {
+        // library.pop()
+        const id = content.dataset.id
+        const idx = library.findIndex(b => b.bookID === id)
+        if (idx !== -1) library.splice(idx, 1)
+        console.log(library)
+        content.remove()
+    })
+    
 }
 
-const del = document.querySelector('.delete')
-
-// del.addEventListener('click', () => {
-//     library.pop()
-// })
